6a3574fcaf5c682716ce8c4efdb90412d79c1965
archanshahh <archanshahh@gmail.com>
2020-04-27 19:04:23 -0400

    update app.js

--- app.js
+++ app.js
@@ -70,11 +70,11 @@ app.get("/api/:game/match/:matchId", (req, res) => {
     });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
